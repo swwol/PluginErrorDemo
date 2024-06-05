5b01e1466373080aa136612ec593f143266f9a3e
sam woolf <sam.woolf@gmail.com>
2024-06-05 14:19:57 +0100

    Add project

---
+++ SwiftGenDemoProject/SwiftGenDemoProject/SwiftGenDemoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftGenDemoProjectApp.swift
+//  SwiftGenDemoProject
+//
+//  Created by Monty on 05/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftGenDemoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
