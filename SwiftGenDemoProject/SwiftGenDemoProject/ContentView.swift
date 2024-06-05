3bf3924df4e19266fd12deb4cbd26aff7269d51d
sam woolf <sam.woolf@gmail.com>
2024-06-05 14:45:25 +0100

    Strings from package

--- SwiftGenDemoProject/SwiftGenDemoProject/ContentView.swift
+++ SwiftGenDemoProject/SwiftGenDemoProject/ContentView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import SwiftGenDemo
 
 struct ContentView: View {
     var body: some View {
@@ -8,6 +9,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
           Text(L10n.projectString1)
           Text(L10n.projectString2)
+          Text(SwiftGenDemo.L10n.string1)
         }
         .padding()
     }
