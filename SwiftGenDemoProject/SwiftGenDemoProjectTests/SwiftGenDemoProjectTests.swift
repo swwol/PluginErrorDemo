4b184f0ffbab46e1bf6274a034d6d4128e3b449b
sam woolf <sam.woolf@gmail.com>
2024-06-06 17:41:47 +0100

    more strings

--- SwiftGenDemoProject/SwiftGenDemoProjectTests/SwiftGenDemoProjectTests.swift
+++ SwiftGenDemoProject/SwiftGenDemoProjectTests/SwiftGenDemoProjectTests.swift
@@ -5,5 +5,6 @@ final class SwiftGenDemoProjectTests: XCTestCase {
 
     func testExample() throws {
       XCTAssertEqual(L10n.projectString1, "Project String 1")
+      XCTAssertEqual(L10n.projectString3, "Project String 3")
     }
 }
