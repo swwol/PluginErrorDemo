4fa050507b2c0665c4297ee5b4c7f5516ac2af38
sam woolf <sam.woolf@gmail.com>
2024-06-06 17:36:45 +0100

    add project test target

---
+++ SwiftGenDemoProject/SwiftGenDemoProjectTests/SwiftGenDemoProjectTests.swift
@@ -0,0 +1,9 @@
+@testable import SwiftGenDemoProject
+import XCTest
+
+final class SwiftGenDemoProjectTests: XCTestCase {
+
+    func testExample() throws {
+      XCTAssertEqual(L10n.projectString1, "Project String 1")
+    }
+}
